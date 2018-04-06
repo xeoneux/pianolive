127d4000849519c379f6e2e1451c4c728f31114d
Aayush Kapoor <xeoneux@icloud.com>
2018-04-06 14:01:16 +0530

    Mirror notes by scale transform

--- src/components/App.js
+++ src/components/App.js
@@ -11,18 +11,18 @@ export default class App extends React.Component {
 
   handleKeyDown = event => {
     let note;
-    if (event.key === 'z' || event.key === 'Z') note = 'b';
-    if (event.key === 's' || event.key === 'S') note = 'as';
-    if (event.key === 'x' || event.key === 'X') note = 'a';
-    if (event.key === 'd' || event.key === 'D') note = 'gs';
-    if (event.key === 'c' || event.key === 'C') note = 'g';
-    if (event.key === 'f' || event.key === 'F') note = 'fs';
+    if (event.key === 'z' || event.key === 'Z') note = 'c';
+    if (event.key === 's' || event.key === 'S') note = 'cs';
+    if (event.key === 'x' || event.key === 'X') note = 'd';
+    if (event.key === 'd' || event.key === 'D') note = 'ds';
+    if (event.key === 'c' || event.key === 'C') note = 'e';
     if (event.key === 'v' || event.key === 'V') note = 'f';
-    if (event.key === 'b' || event.key === 'B') note = 'e';
-    if (event.key === 'h' || event.key === 'H') note = 'ds';
-    if (event.key === 'n' || event.key === 'N') note = 'd';
-    if (event.key === 'j' || event.key === 'J') note = 'cs';
-    if (event.key === 'm' || event.key === 'M') note = 'c';
+    if (event.key === 'g' || event.key === 'G') note = 'fs';
+    if (event.key === 'b' || event.key === 'B') note = 'g';
+    if (event.key === 'h' || event.key === 'H') note = 'gs';
+    if (event.key === 'n' || event.key === 'N') note = 'a';
+    if (event.key === 'j' || event.key === 'J') note = 'as';
+    if (event.key === 'm' || event.key === 'M') note = 'b';
 
     if (note) {
       if (note.includes('s')) note = note[0] + '#';
